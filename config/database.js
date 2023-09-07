c5226a8bb138e99a618a3bafe2d4c1a8b26c328b
Kiên Nguyễn <108124347+KienNguyen1306@users.noreply.github.com>
2023-09-07 19:44:18 +0700

    host

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME,
-  // host: 'localhost'
+  host: '198.54.114.158'
 });
 
 module.exports = sequelize;
