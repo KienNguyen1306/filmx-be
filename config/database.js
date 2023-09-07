ac399e652cc549556ccc44f7bff7472671ae76bf
Kiên Nguyễn <108124347+KienNguyen1306@users.noreply.github.com>
2023-09-07 19:36:51 +0700

    data

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  dialect: 'mysql',
-  username: 'root',
-  password: '',
-  database: 'filmx',
-  host: 'localhost'
+  dialect: "mysql",
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  // host: 'localhost'
 });
 
 module.exports = sequelize;
