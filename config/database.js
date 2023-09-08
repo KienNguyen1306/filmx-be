5c994ec3fd77340b276e33b9ca420f6a88dbcbcf
Kiên Nguyễn <108124347+KienNguyen1306@users.noreply.github.com>
2023-09-08 09:55:42 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require("sequelize");
-
+const dotenv = require("dotenv");
+dotenv.config(); // Đọc biến môi trường từ tệp .env
 const sequelize = new Sequelize({
   dialect: "mysql",
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
-  port:process.env.DATABASE_PORT
+  port: process.env.DATABASE_PORT,
 });
+console.log("password", process.env.DATABASE_PASS);
 
 module.exports = sequelize;
